19421958703df350c6f729a02e4abd16cc40b24d
Felipe Martin <FelipeTheFeh@gmail.com>
2026-03-16 03:32:26 -0300

    Atualiza configurações de teste para Vitest, ajusta importações e inclui arquivo de teste no TypeScript

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
 import { localStorageMock } from './src/mocks/localStorage';
 
 Object.defineProperty(window, 'localStorage', {
